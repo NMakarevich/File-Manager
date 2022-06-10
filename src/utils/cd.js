baa26d27f803ae3d7b4b042ca4c8c46a3e55cfbd
Nikolai Makarevich <nikoladze529@mail.ru>
2022-06-10 20:35:56 +0300

    refactor: simplify cd function

--- src/utils/cd.js
+++ src/utils/cd.js
@@ -4,14 +4,9 @@ import {__dirname} from "../index.js";
 
 async function cd([pathToDirectory]) {
     try {
-        if (path.isAbsolute(pathToDirectory)) {
-            await access(pathToDirectory);
-            return pathToDirectory;
-        } else {
-            const fullPath = path.join(__dirname, pathToDirectory);
-            await access(fullPath);
-            return fullPath;
-        }
+        pathToDirectory = path.isAbsolute(pathToDirectory) ? pathToDirectory : path.join(__dirname, pathToDirectory);
+        await access(pathToDirectory);
+        return pathToDirectory;
     } catch (err) {
         console.log('\x1b[1;31mOperation failed\x1b[0m');
         return __dirname;
